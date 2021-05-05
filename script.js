1d678f54433c2b9be7758ef3b53902b3fd994c19
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2021-05-05 14:40:44 +0200

    All new files
    Responsive - everything....I hope!

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
+window.onload= function () {
+    window.scrollTo(0, 0);
+};
+
 const burgerButton = document.getElementsByClassName('burger')[0];
 const menuBar = document.getElementsByClassName('menu-bar')[0]
-const menuLink = document.getElementsByClassName('menu-bar ul');
+const menuLink = document.getElementById('click-control');
 
 
-burgerButton.addEventListener('click', ()=>{
+burgerButton.addEventListener('click', toggleButton);
+menuLink.addEventListener('click', toggleButton);
+
+function toggleButton (){
     menuBar.classList.toggle('active');
+}
+
+
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+const messageBox = document.getElementById('insert-message');
+const nameBox = document.getElementById('insert-name');
+const mailBox = document.getElementById('insert-mail');
+
+submit.addEventListener('click', validateMessage);
+
+function validateMessage(e){
+    e.preventDefault()
+    if(messageBox.value !== '' && nameBox.value !== '' && mailBox.value !== ''){
+        alert('\nMessage sent!\n\nThank you for time!');
+        window.location.reload();
+    } else {
+        alert('\nMessage NOT sent!!!\n\nFill in all the fields in the contact form!');   
+        
+}}
     
-})
+const topButton = document.getElementById("button-top");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+
+
+    
